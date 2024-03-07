c7e84e2cddbac63a92185ba15cbb1f90fd1ac094
monsterhunters <88552297+monsterhunters@users.noreply.github.com>
2024-03-07 19:53:27 +0630

    Add files via upload

---
+++ custom.py
@@ -0,0 +1,78 @@
+import os
+
+def installAnimateDiff():
+      os.system('git clone https://github.com/continue-revolution/sd-webui-animatediff.git')
+
+def installtext2video():
+      os.system('git clone https://github.com/kabachuha/sd-webui-text2video')
+
+def installReactor():
+      os.system('git clone https://github.com/Gourieff/sd-webui-reactor')
+
+def installDeforum():
+       os.system('git clone https://github.com/deforum-art/sd-webui-deforum.git')
+
+def highRes():
+       os.system('git clone https://github.com/klimaleksus/stable-diffusion-webui-conditioning-highres-fix.git')
+
+def twoShot():
+       os.system('git clone https://github.com/opparco/stable-diffusion-webui-two-shot.git')
+
+def composableLora():
+       os.system('git clone https://github.com/opparco/stable-diffusion-webui-composable-lora.git')
+
+def locon():
+       os.system('git clone https://github.com/KohakuBlueleaf/a1111-sd-webui-locon.git')
+
+def llul():
+       os.system('git clone https://github.com/hnmr293/sd-webui-llul.git')
+
+def ControlNet():
+       os.system('git clone https://github.com/Mikubill/sd-webui-controlnet.git')
+
+def installRegionalPrompter():
+       os.system('git clone https://github.com/hako-mikan/sd-webui-regional-prompter')
+
+def installUltimateSDUpscale():
+       os.system('git clone https://github.com/Coyote-A/ultimate-upscale-for-automatic1111')
+
+def installOpenPoseEditor():
+       os.system('git clone https://github.com/fkunn1326/openpose-editor')
+
+def installADetailer():
+       os.system('git clone https://github.com/Bing-su/adetailer')
+
+def installSadtalker():
+        os.system('git clone https://github.com/OpenTalker/SadTalker.git')
+
+def installFacechain():
+        os.system('git clone https://github.com/modelscope/facechain.git')
+
+def installRoop():
+        os.system('git clone https://github.com/s0md3v/sd-webui-roop')
+
+def installDreambooth():
+        os.system('https://github.com/d8ahazard/sd_dreambooth_extension.git')
+      
+def installExt():
+        os.system('git clone https://github.com/aigc-apps/sd-webui-EasyPhoto.git')
+        os.system('git clone https://github.com/Scholar01/sd-webui-mov2mov.git')
+        os.system('git clone https://github.com/AUTOMATIC1111/stable-diffusion-webui-wildcards.git')
+        os.system('git clone https://github.com/CiaraStrawberry/TemporalKit')
+        os.system('git clone https://github.com/huchenlei/sd-webui-openpose-editor.git')
+        os.system('git clone https://github.com/continue-revolution/sd-webui-segment-anything.git')
+        os.system('git clone https://github.com/Bing-su/adetailer.git')
+        os.system('git clone https://github.com/Physton/sd-webui-prompt-all-in-one')
+        os.system('git clone https://github.com/zanllp/sd-webui-infinite-image-browsing.git')
+        os.system('git clone https://github.com/v8hid/infinite-zoom-automatic1111-webui.git')
+        os.system('git clone https://github.com/hako-mikan/sd-webui-regional-prompter.git')
+        os.system('git clone https://github.com/pkuliyi2015/multidiffusion-upscaler-for-automatic1111.git')
+        os.system('git clone https://github.com/s9roll7/ebsynth_utility.git')
+        os.system('git clone https://github.com/fkunn1326/openpose-editor.git')
+        os.system('git clone https://github.com/mcmonkeyprojects/sd-dynamic-thresholding.git')
+        os.system('git clone https://github.com/Coyote-A/ultimate-upscale-for-automatic1111.git')
+        os.system('git clone https://github.com/kohya-ss/sd-webui-additional-networks.git')
+        os.system('git clone https://github.com/thygate/stable-diffusion-webui-depthmap-script.git')
+        os.system('git clone https://github.com/DominikDoom/a1111-sd-webui-tagcomplete.git')
+      
+
